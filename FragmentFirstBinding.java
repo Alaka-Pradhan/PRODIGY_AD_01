2817e933d5005d0d6a42719bd2191d30dc992148
Alaka-Pradhan <pradhanalaka1976@gmail.com>
2024-09-04 20:53:34 +0530

    Add files via upload

---
+++ FragmentFirstBinding.java
@@ -0,0 +1,18 @@
+package com.example.calculator;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentFirstBinding {
+    public View buttonFirst;
+    public View buttonSecond;
+
+    public static FragmentFirstBinding inflate(LayoutInflater inflater, ViewGroup container, boolean b) {
+        return null;
+    }
+
+    public View getRoot() {
+        return null;
+    }
+}
